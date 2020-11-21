fb75aef5a270d52e051a84c034431198ccb9b143
CoderBee <revatid15@gmail.com>
2020-11-21 10:35:11 +0530

    website

---
+++ includes/head.php
@@ -0,0 +1,43 @@
+<head>
+  <title>Organic Store Agriculture Category bootstrap Responsive web Template| Home :: w3layouts</title>
+  <!--meta tags -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="Organic Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+         Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+  />
+  <script>
+    addEventListener("load", function () {
+      setTimeout(hideURLbar, 0);
+    }, false);
+
+
+    function hideURLbar() {
+      window.scrollTo(0, 1);
+    }
+  </script>
+  <!--booststrap-->
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+  <!--//booststrap end-->
+  <!-- font-awesome icons -->
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <!-- //font-awesome icons -->
+  <!--stylesheets-->
+  <link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+  <!--//stylesheets-->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+
+
+  <style>
+      .unselectable {
+        -webkit-user-select: none;
+        -webkit-touch-callout: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        color: #cc0000;
+      }
+    </style>
+</head>
---
+++ includes/navbar.php
@@ -0,0 +1,61 @@
+<nav>
+  <div class="container" style="max-width: 100%">
+    <div class="row">
+      <div class="col-lg-2 logo">
+        
+        <img src="images/logo.png" width="40%">
+
+      </div>
+
+      <div class="col-lg-10" style="padding: 1em 2em;">
+        <label for="drop" class="toggle">Menu</label>
+        <input type="checkbox" id="drop">
+        <ul class="menu mt-2">
+          <li class="active">
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#service">Services</a>
+          </li>
+          <li>
+            <!-- First Tier Drop Down -->
+            <!--<label for="drop-2" class="toggle toogle-2">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span>
+              </label>
+              <a href="#">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span></a>
+              <input type="checkbox" id="drop-2">
+              <ul>
+                <li><a href="gallery.html" class="drop-text">Gallery</a></li>
+                <li><a href="menu.html" class="drop-text">Menu</a></li>
+                <li><a href="recipe.html" class="drop-text">Recipes</a></li>
+              </ul>
+            </li>-->
+            <li>
+              <a href="#gallery">Gallery</a>
+            </li>
+            <li>
+              <a href="#blog">Blog</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+    
+  </div>
+        <!-- <div id="logo">
+          <h1>
+            <a href="index.html"><img src="images/logo.png" width="10%"></a>
+          </h1>
+        </div> -->
+        <!-- <div class="sub-headder position-relative">
+          <h6>
+            <a href="index.html">Organic
+              <br>Store</a>
+          </h6>
+        </div> -->
+        
+      </nav>
